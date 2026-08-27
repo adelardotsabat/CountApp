31a4b5c5b0cb7723d7a8b8f2a94d50229024b813
Adelardo <adelardotsabatchalad@mail.ugm.ac.id>
2026-08-27 08:59:29 +0700

    Layout Update

--- app/src/main/java/com/example/countapp/MainActivity.kt
+++ app/src/main/java/com/example/countapp/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.countapp
 
+import android.os.Binder
 import android.os.Bundle
+import android.view.inputmethod.InputBinding
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.countapp.databinding.ActivityMainBinding
+
+// bikim variabel untuk binding
+private lateinit var binding: ActivityMainBinding
+private var number = 0
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // inisiasi binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // set content dari activity untuk menggunakan binding view root
+        setContentView(binding.root)
+
+        // sekarang
+        with(receiver = binding) {
+
+
         }
     }
 }
